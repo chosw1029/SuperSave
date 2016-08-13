9831683667f53f274f21bd23a16543bea4adfb38
ReStartAllKill_note <chosw10293@naver.com>
2016-08-13 17:29:28 +0900

    hh

---
+++ SuperSave2/app/src/main/java/com/nextus/supersave/MyApplication.java
@@ -0,0 +1,38 @@
+package com.nextus.supersave;
+
+import android.app.Application;
+
+import com.nextus.supersave.db.CustomSQLiteHelper;
+
+/**
+ * Created by chosw on 2016-08-03.
+ */
+public class MyApplication extends Application {
+
+    public static MyApplication mInstance;
+    private static CustomSQLiteHelper helper;
+    public int total_kwh = 0;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        mInstance = this;
+        helper = new CustomSQLiteHelper( this, "ELECTRO_DATA.db", null, 1);
+    }
+
+    public CustomSQLiteHelper getHelper()
+    {
+        return helper;
+    }
+
+    public int getTotal_kwh()
+    {
+        return total_kwh;
+    }
+
+    public void setTotal_kwh(int kwh)
+    {
+        total_kwh = kwh;
+    }
+
+}
